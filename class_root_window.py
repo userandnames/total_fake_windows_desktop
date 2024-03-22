dcc4c61725e1413e107c8ac9ec73033bb21b6bd7
yanyuchen <20225360@stu.neu.edu.cn>
2024-03-22 15:44:59 +0800

    3

--- class_root_window.py
+++ class_root_window.py
@@ -2,16 +2,21 @@ import tkinter as tk
 import os
 import json
 from PIL import Image, ImageTk
-
-img = None
+from pynput import mouse
 
 
 class Fake_Win:
     def __init__(self, main_window):
+        listener = mouse.Listener(
+            on_click=self.simulate_left_pressed)
+        listener.start()
+
         self.root_path = ''
         self.setting_path = ''
         self.settings = dict()
         self.background_photo = None
+        self.pressed_position = []
+        self.selected_rectangle = None
 
         self.init_settings()  # 初始化路径设置
 
@@ -19,8 +24,9 @@ class Fake_Win:
         self.size = [self.root.winfo_screenwidth(), self.root.winfo_screenheight()]
         self.canvas_root = tk.Canvas(self.root, width=int(self.size[0]), height=int(self.size[1]), bg="black")
         self.canvas_root.place(x=0, y=0)
+        self.right_click_bar = None
         if self.set():
-            self.set_background(self.settings["background_pic"])
+            self.set_canvas_and_background_init()
 
     def init_settings(self):
         self.root_path = os.getcwd()
@@ -31,27 +37,57 @@ class Fake_Win:
                 self.settings[i] = os.listdir(self.setting_path + "\\" + i)
 
     def set(self):
-        # self.root.attributes('-fullscreen', True)
-        self.root.geometry('1200x699')
+        self.root.attributes('-fullscreen', True)
+        self.root.overrideredirect(True)
         file1 = self.setting_path + "\\settings.json"
         if os.path.exists(file1) and os.path.isfile(file1):
             with open(file1, 'r') as f:
                 data = json.load(f)
-                background_pic = self.setting_path + "\\desktop_backgrounds\\" + data['background']
+                background_pic = self.setting_path + "\\desktop_backgrounds\\gif\\" + data['background']
                 self.settings["background_pic"] = background_pic
+                print(self.settings["background_pic"])
+                return 1
         else:
             print("File " + str(file1) + " does not exist\n"+"in path "+str(file1))
             self.shutdown()
             return 0
 
-    def set_background(self, background_pic):
-        self.background_photo = tk.PhotoImage(file=background_pic)
+    def set_canvas_and_background_init(self):
+        print("222"+self.settings["background_pic"])
+        self.background_photo = Image.open(self.settings["background_pic"])
+        self.background_photo = self.background_photo.resize((self.size[0], self.size[1]))
+        self.background_photo = ImageTk.PhotoImage(self.background_photo)
+        # self.background_photo = tk.PhotoImage(file=self.settings["background_pic"])
         self.canvas_root.place(x=0, y=0)
-
-        self.canvas_root.create_line(0, 0, 200, 200, fill='blue')
-        self.canvas_root.create_rectangle(50, 50, 150, 150, fill='blue', outline='green', width=2)
-
         self.canvas_root.create_image(int(self.size[0])/2, int(self.size[1])/2, image=self.background_photo)
+        self.canvas_root.bind("<Button-3>", self.simulate_right_click)
+        self.canvas_root.bind("<Button-1>", self.simulate_left_click)
+        self.canvas_root.bind("<ButtonPress-1>", self.simulate_left_pressed)
+
+    def simulate_right_click(self, event):
+        x = event.widget.winfo_pointerx()
+        y = event.widget.winfo_pointery()
+        if self.right_click_bar is not None:
+            self.right_click_bar.destroy()
+        self.right_click_bar = tk.Toplevel(self.canvas_root)
+        self.right_click_bar.geometry(f"{200}x{400}+{x}+{y}")
+        menu_bar = tk.Menu(self.right_click_bar)
+
+    def simulate_left_click(self, event):
+        if self.right_click_bar is not None:
+            self.right_click_bar.destroy()
+
+    def simulate_left_pressed(self, event):
+        x = event.widget.winfo_pointerx()
+        y = event.widget.winfo_pointery()
+        if len(self.pressed_position) == 0:
+            self.pressed_position.append([x, y])
+            self.pressed_position.append([x, y])
+        elif len(self.pressed_position) >= 2:
+            self.pressed_position[-1] = [x, y]
+            self.selected_rectangle = self.canvas_root.create_rectangle(self.pressed_position[0][0], self.pressed_position[0][1],
+                                                          self.pressed_position[-1][0], self.pressed_position[-1][1],
+                                                          fill="blue", outline="blue", stipple="gray25")
 
     def maintain(self):
         self.root.mainloop()
--- settings/desktop_backgrounds/try_changed.py
+++ settings/desktop_backgrounds/try_changed.py
@@ -5,16 +5,16 @@ class MyWindow:
     def __init__(self, master):
         self.img = None
         self.master = master
-        self.canvas = tk.Canvas(self.master, width=200, height=200, bg='white')
+        self.canvas = tk.Canvas(self.master, width=400, height=400, bg='black')
         self.set()
 
     def set(self):
         self.master.geometry('1200x699')
-        self.create_canvas()
+        self.create_canvas("gif/changed.gif")
 
-    def create_canvas(self):
+    def create_canvas(self, path):
         self.canvas.place(x=50, y=50)  # 使用place方法将Canvas放置到窗口上
-        self.img = tk.PhotoImage(file="changed.gif")
+        self.img = tk.PhotoImage(file=path)
         self.canvas.create_image(100, 100, image=self.img)  # 使用create_image方法在Canvas上添加图片
         self.canvas.create_line(0, 0, 200, 200, fill='blue')
         self.canvas.create_rectangle(50, 50, 150, 150, fill='red')
